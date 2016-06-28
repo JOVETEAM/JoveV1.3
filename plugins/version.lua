cf839bdd14da1e95a42f2dbc0e5be9ab35319d6f
Pouya Poorrahman <poyaskysps@gmail.com>
2016-06-28 22:44:46 +0430

    Create version.lua

---
+++ plugins/version.lua
@@ -0,0 +1,43 @@
+
+do
+
+function run(msg, matches)
+  return [[ 
+Smart is an advanced and powerful administration bot based on TeleSeed and Copy from Mega Shield
+CPU Core Model:Intel(R) Core(TM) i7-6700 CPU @ 3.40GHz
+Cores:3 Core
+Ram:4 Gig
+HDD:60 Gig SSD
+Location Server:Hetzner Germany
+Team Channel: @SmartTG_CH
+Configure: @XzX_ERPO_XzX
+Manager: @XeX_ERPO_XeX
+Admin: @Us_Mm
+Powerd by:
+ #SmartTG
+ 
+Special Thanks:
+@theking_1995
+Morteza No PV(Mega Sheild)
+Hafez(Mega Shild)
+ and other people...]]
+end
+
+return {
+  description = "Shows bot version", 
+  usage = "ver: Shows bot version",
+  patterns = {
+    "^[Vv]er$",
+    "^[Vv]ersion$",
+    "^!smart$",
+    "^%smart$",
+    "^$smart$",
+   "^#Smart$",
+   "^#Smart$",
+   "^/smart$",
+   "^#smart$",
+  }, 
+  run = run 
+}
+
+end
