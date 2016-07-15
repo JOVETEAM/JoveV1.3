7dace55abfaa3582ac9ffe212e98481cd4230817
Pouya Poorrahman <poyaskysps@gmail.com>
2016-07-15 15:29:41 +0430

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,10 +91,10 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'megasmart' then -- Put everything you like :)
+  if matches[1]:lower() == 'phoenix' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
-    savelog(msg.to.id, name.." ["..msg.from.id.."] used /megasmart ")
+    savelog(msg.to.id, name.." ["..msg.from.id.."] used /phoenix ")
     return about
   end 
   if matches[1]:lower() == "statslist" then
@@ -121,7 +121,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "megasmart" then -- Put everything you like :)
+    if matches[2] == "phoenix" then -- Put everything you like :)
       if not is_admin1(msg) then
         return "For admins only !"
       else
@@ -143,8 +143,8 @@ return {
     "^[#!/]([Ss]tats)$",
     "^[#!/]([Ss]tatslist)$",
     "^[#!/]([Ss]tats) (group) (%d+)",
-    "^[#!/]([Ss]tats) (megasmart)",
-	"^[#!/]([Mm]egasmart)"
+    "^[#!/]([Ss]tats) (phoenix)",
+	"^[#!/]([Pp]hoenix)"
     }, 
   run = run
 }
