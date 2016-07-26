de48a8349bfe0be8979240ab679bf8153ee3fa78
Pouya Poorrahman <poyaskysps@gmail.com>
2016-07-26 19:55:45 +0430

    Update write.lua

--- plugins/write.lua
+++ plugins/write.lua
@@ -106,7 +106,7 @@ local function run(msg, matches)
 		a=a+1
 		result_text = result_text..a.."- "..result[a].."\n\n"
 	end
-	return result_text.."______________________________\n#SmartTG"
+	return result_text.."______________________________\n#JoveTG"
 end
 
 return {
