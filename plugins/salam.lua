4b1cdcfc51b2b300adbe0f58e13392d9f5d74ba4
Pouya Poorrahman <poyaskysps@gmail.com>
2016-07-26 17:53:08 +0430

    Jove V1.3

--- plugins/salam.lua
+++ plugins/salam.lua
@@ -2,8 +2,8 @@ do
 
 function run(msg, matches)
 local reply_id = msg['id']
-local text = 'ڛـلام بابايےツ'
-if matches[1] == 'سلام پسرم' or 'سلام' or 'slm' or 'Hello dadi' then
+local text = 'سـلام بـابـاツ'
+if matches[1] == 'سلام پسرم' or 'سلام' or 'salam' or 'slm' then
     if is_sudo(msg) then
 reply_msg(reply_id, text, ok_cb, false)
 end
@@ -13,8 +13,8 @@ return {
 patterns = {
     "^سلام پسرم$",
     "^سلام$",
-    "^slm$",
-    "^Hello dadi$"
+    "^salam$",
+    "^slm$"
 },
 run = run
 }
