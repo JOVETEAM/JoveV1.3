f18767da8dbbf20c5cdcbd5a6c0f9fc8672dfc62
Pouya Poorrahman <poyaskysps@gmail.com>
2016-07-26 17:35:10 +0430

    Jove V1.3

--- plugins/linkpv.lua
+++ plugins/linkpv.lua
@@ -7,7 +7,7 @@ function run(msg, matches)
     local data = load_data(_config.moderation.data)
       local group_link = data[tostring(msg.to.id)]['settings']['set_link']
        if not group_link then 
-        return "اول باید لینک جدید ایجاد کنید"
+        return "اول باید لینک جدید ایجاد کنی"
        end
          local text = "لینک گروه:\n"..group_link
           send_large_msg('user#id'..msg.from.id, text, ok_cb, false)
