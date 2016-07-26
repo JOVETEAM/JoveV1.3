410aed2b14c17affd09a671dbf71628a71259759
Pouya Poorrahman <poyaskysps@gmail.com>
2016-07-26 17:51:11 +0430

    Jove V1.3

--- plugins/set_type.lua
+++ plugins/set_type.lua
@@ -9,11 +9,11 @@ if matches[1] and is_sudo(msg) then
 data[tostring(msg.to.id)]['group_type'] = matches[1]
         save_data(_config.moderation.data, data)
         
-        return 'Group Type Seted To : '..matches[1]
+        return 'نوع گروه شد این : '..matches[1]
 
 end
 if not is_owner(msg) then 
-    return 'You Are Not Allow To set Group Type !'
+    return 'الکی زور نزن نمیتونی نوع گروه رو عوض کنی !'
     end
 end
 return {
