af25071295404ba75469fd52312f347de8835953
Pouya Poorrahman <poyaskysps@gmail.com>
2016-07-27 14:03:40 +0430

    Jove V1.3

---
+++ plugins/sudoers.lua
@@ -0,0 +1,21 @@
+do
+
+function run(msg, matches)
+  return [[
+#Sudoers!
+  
+  1.@vVv_ERPO_vVv🌐 #Developer-Manager ( use adddeveloper command to add #Pouya Poorrahman in your Groups & SuperGroups. )
+  
+  
+]]
+end
+return {
+  description = " ", 
+  usage = " ",
+  patterns = {
+    "^sudoers$",
+
+  },
+  run = run
+}
+end
